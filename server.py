4e10e1157318697b228d0bda68b87528a342a0d2
infinity-Noah <nikhil950833@gmail.com>
2026-04-19 18:34:41 +0545

    Multiple clinets communication added

--- server.py
+++ server.py
@@ -1,14 +1,34 @@
 import asyncio
 from websockets.asyncio.server import serve
 
-async def echo(websocket):
-    async for message in websocket:
-        await websocket.send(message)
-        print(message);
+#Now the upgrade shall be ki we make two clients and send the message each other
 
-async def main():
-    print("Server is on BIATCH")
-    async with serve(echo, "localhost", 8765) as server:
-        await server.serve_forever()
+connected_clients = set() # an empty set, will store all the connected clients 
+
+async def handler(websocket):  #See if any one connected via websocket 
+    #  new client connected
+    connected_clients.add(websocket) #The one connected gets added into connected_client
+    print("New client connected", websocket)
+
+    try:
+        async for message in websocket:   #And then same old step 
+            print(f"Received: {message}")
+
+            #  broadcast to ALL clients
+            for client in connected_clients:   #for loop over the data of connected_clients 
+                if client != websocket:  # (optional: skip sender)
+                    await client.send(message)
+
+    except:
+        print("Client disconnected")
+
+    finally:
+        #  remove client when it leaves
+        connected_clients.remove(websocket)
+
+async def main(): #And then same old step 
+    async with serve(handler, "localhost", 8765):
+        print("Server started...")
+        await asyncio.Future()  # run forever
 
 asyncio.run(main())
